a07db1fdd23a1dc0c73c238e6995019150bc015e
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-01-26 16:12:26 +0530

    test case handled for  returns 0 and number returns number

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe StringCalculator, type: :service do
+  RSpec.describe StringCalculator, type: :service do
   describe '#add' do
-    it 'returns 0 for an empty string' do
-      expect(StringCalculator.new.add("")).to eq(0)
-    end
+  it 'returns 0 for an empty string' do
+  expect(StringCalculator.new.add("")).to eq(0)
   end
-end
+  it 'returns the number itself for a single number' do
+  expect(StringCalculator.new.add("5")).to eq(5)
+  end
+  end
+  end
