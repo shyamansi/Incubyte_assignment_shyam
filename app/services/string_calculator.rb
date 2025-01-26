a805382f6cc5bb21e0b396dc3fb93ffd4a1d6baf
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-01-26 16:17:09 +0530

    handled comma separated numbers

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,5 +2,6 @@ class StringCalculator
   def add(numbers)
      return 0 if numbers.empty?
      return numbers.to_i if numbers.match(/^\d+$/)
+     numbers.split(",").map(&:to_i).sum
   end
 end
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
   RSpec.describe StringCalculator, type: :service do
-  describe '#add' do
-  it 'returns 0 for an empty string' do
-  expect(StringCalculator.new.add("")).to eq(0)
-  end
-  it 'returns the number itself for a single number' do
-  expect(StringCalculator.new.add("5")).to eq(5)
-  end
-  end
+    describe '#add' do
+      it 'returns 0 for an empty string' do
+        expect(StringCalculator.new.add("")).to eq(0)
+      end
+      it 'returns the number itself for a single number' do
+        expect(StringCalculator.new.add("5")).to eq(5)
+      end
+      it 'returns the sum of two comma-separated numbers' do
+       expect(StringCalculator.new.add("1,2")).to eq(3)
+      end
+
+    end
   end
